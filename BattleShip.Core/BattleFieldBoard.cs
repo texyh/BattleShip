f86ad9a82a4e5d0302377315fd89df60190d4099
Onwuzulike Emeka <onwuzulikee1@gmail.com>
2020-12-29 18:23:59 +0100

    first commit

---
+++ BattleShip.Core/BattleFieldBoard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BattleShip.Core
+{
+    public class BattleFieldBoard
+    {
+        private const int GAMEROWS = 10;
+
+        private const int GAMECOLUMS = 10;
+
+        private const int EMPTYSPACE = 0;
+
+        public int[,] OceanGrid = new int[GAMEROWS, GAMECOLUMS];
+
+        public BattleFieldBoard()
+        {
+        }
+    }
+}
---
+++ BattleShip.Tests/BattleShipTests.cs
@@ -0,0 +1,41 @@
+﻿using BattleShip.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BattleShip.Tests
+{
+    public class BattleShipTests
+    {
+        [Fact]
+        public void OceanGrid_Should_Be_Initialized_When_Game_IsCreated()
+        {
+            var game = new BattleFieldBoard();
+
+            Assert.Equal(10, game.OceanGrid.GetLength(0));
+            Assert.Equal(10, game.OceanGrid.GetLength(1));
+        }
+
+        [Fact]
+        public void OceanGroid_Should_Have_One_BattleShip_When_Game_IsCreated()
+        {
+            var game = new BattleFieldBoard();
+            var expectedNumberOfBattleShipSquares = 5;
+            var totalNumberOfBattleShip = 0;
+
+            for (int row = 0; row < 10; row++)
+            {
+                for (int column = 0; column < 10; column++)
+                {
+                    if(game.OceanGrid[row, column] == 2)
+                    {
+                        totalNumberOfBattleShip++;
+                    }
+                }
+            }
+
+            Assert.Equal(expectedNumberOfBattleShipSquares, totalNumberOfBattleShip);
+        }
+    }
+}
